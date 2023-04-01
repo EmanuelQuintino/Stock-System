4fe2a7dd7695e418e4911f0e057a1ba8e983f834
EmanuelQuintino <emanuelquintino@hotmail.com>
2023-04-01 15:26:47 -0300

    center add product icon

--- client/src/components/InputSearch/style.js
+++ client/src/components/InputSearch/style.js
@@ -1,6 +1,8 @@
 import styled from "styled-components";
 
 export const Container = styled.section `
+    text-align: center;
+    
     .inputSection {
         position: relative;
         max-width: 42.0rem;
