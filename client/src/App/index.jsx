d430b9dcb61f54f3a13056741e9d02525c9e4607
EmanuelQuintino <emanuelquintino@hotmail.com>
2023-04-04 18:19:55 -0300

    add text img alt

--- client/src/App/index.jsx
+++ client/src/App/index.jsx
@@ -6,14 +6,14 @@ export function App() {
   return (
     <Container>
       <header>
-        <img src="" alt="" />
+        <img src="" alt="image-header" />
       </header>
       <main>
         <h1>Stock-System</h1>
         <HandleProducts/>
       </main>
       <footer>
-        <img src="" alt="" />
+        <img src="" alt="image-footer" />
       </footer>
     </Container>    
   )
